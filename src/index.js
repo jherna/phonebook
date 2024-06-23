e8e3a16a214b07eb008f44a9a3d84a1003555f63
jherna <jherna76@elpuig.xeill.net>
2024-06-23 16:08:58 +0200

    exercises 3.6 step 6 completed add POST manage errors with no names or no number or if name already exist

--- src/index.js
+++ src/index.js
@@ -55,6 +55,38 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+//add a new note
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body['number']) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+
+    const person = {
+        name: body.name,
+        number: body['number'],
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
