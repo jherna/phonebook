101af98261d60b1d2b755c341a4ba9bba74b3dc9
jherna <jherna76@elpuig.xeill.net>
2024-06-23 14:22:45 +0200

    exercises 3.4 step 4 completed add get a single person add delete a single person

--- src/index.js
+++ src/index.js
@@ -37,6 +37,24 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
+//get a single person by id
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+//delete a single note by id
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
