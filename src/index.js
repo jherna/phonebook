d72f06a80fb9f8f4c1b6f14f7fb07939a322bbbc
jherna <jherna76@elpuig.xeill.net>
2024-06-27 17:25:54 +0200

    exercise 3.19 step 7 completed  - validations errors  - field name required

--- src/index.js
+++ src/index.js
@@ -89,11 +89,11 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    if (!body.name || !body['number']) {
+    /*if (!body.name || !body['number']) {
         return response.status(400).json({
             error: 'name or number missing'
         })
-    }
+    }*/
 
     const person = new Person({
         name: body.name,
@@ -117,8 +117,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } else {
-        return response.status(400).send({ error: 'name or number missing' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
     }
 
     next(error)
