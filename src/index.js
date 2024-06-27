bb306aedcc15b82717ce4f1d07936e80551f7026
jherna <jherna76@elpuig.xeill.net>
2024-06-27 16:16:18 +0200

    exercise 3.18 step 6 completed  - add update person and findbyId

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+
+
 let persons = [
     {
         "id": 1,
@@ -51,27 +53,41 @@ app.get('/info', (request, response) => {
 })
 
 //get a single person by id
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             if(person) response.json(person)
             else response.status(404).end()
     }).catch(error => {
-        console.log(error)
-        response.status(400).send({error: 'malformatted id'})
+        next(error)
     })
 })
 
 //delete a single note by id
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(person => {
             response.status(204).end()
         })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 
-app.post('/api/persons', (request, response) => {
+
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body.name || !body['number']) {
         return response.status(400).json({
@@ -86,7 +102,7 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
     /*
     if (persons.find(person => person.name === body.name)) {
         return response.status(400).json({
@@ -95,6 +111,19 @@ app.post('/api/persons', (request, response) => {
     }
     */
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else {
+        return response.status(400).send({ error: 'name or number missing' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
